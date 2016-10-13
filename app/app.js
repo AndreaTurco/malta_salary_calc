dddca719f028d6e4329dc9283047178b1b0f5127
andy mac <sigturk@gmail.com>
2016-10-13 08:02:07 +0200

    added slider angular

--- app/app.js
+++ app/app.js
@@ -2,11 +2,20 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute', 'chart.js'
+  'ngRoute', 'chart.js','rzModule'
 ])
 .controller("DoughnutCtrl", function ($scope) {
   $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.salary = 10000;
+  $scope.slider = {
+	  minValue: 10000,
+	  options : {
+		draggableRange: false,
+		step : 1000,
+		vertical : true,
+		minlimit : 0,
+		maxlimit : 300000
+	  }
+	};
   $scope.data = [$scope.salary, 500, 100];
 });
 /*.
